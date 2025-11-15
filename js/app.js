e40eba1b4e04d08d7b13034f383d97f51ef20d05
Rahbster <rahbster@yahoo.com>
2025-11-15 18:26:23 -0500

    PWA Fix attempt

--- js/app.js
+++ js/app.js
@@ -96,7 +96,7 @@ async function initializePWAUpdater() {
     let newWorker;
 
 
-    const registration = await navigator.serviceWorker.register('sw.js');
+    const registration = await navigator.serviceWorker.register('/sw.js');
     // console.log('ServiceWorker registration successful with scope: ', registration.scope);
 
     // Listen for a new service worker being installed.
@@ -134,7 +134,7 @@ async function initializePWAUpdater() {
             console.warn('Service worker registration not active. Cannot check for updates.');
             showToast('Update check is not available in this context.', 'error');
             // Attempt to re-register if something went wrong.
-            navigator.serviceWorker.register('sw.js');
+            navigator.serviceWorker.register('/sw.js');
         }
     };
 }
